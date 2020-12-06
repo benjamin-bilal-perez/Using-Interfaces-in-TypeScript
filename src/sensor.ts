c21a734f82b306a372e1d3dba3b16d6f404143d7
Benjamín Bilal <56590175+benjamin-bilal-perez@users.noreply.github.com>
2020-12-06 19:34:02 +0000

    Update sensor.ts

--- src/sensor.ts
+++ src/sensor.ts
@@ -1,15 +1,24 @@
 import { ISensor } from "./ISensor";
 
 class Sensor implements ISensor {
-    private _humidity: number;
+    private _humidity:number = Math.floor(Math.random() * (100 - 0)) + 0;
+    private _fluctuation: number = 0;
 
     read() {
-        this._humidity = Math.floor(Math.random() * (100 - 0)) + 0;
+        this._humidity += this._fluctuation;
     }
 
     get humidity(): number {
         return this._humidity;
     }
+
+    get fluctuation(): number {
+        return this._fluctuation;
+    }
+    set fluctuation(fluctuation: number) {
+        this._fluctuation = fluctuation;
+    }
+    
 }
 
 export { Sensor };
