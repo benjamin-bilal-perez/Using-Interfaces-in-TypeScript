cd648aac55afcc10b3882688019e7db65b24ccce
Benjamín Bilal <56590175+benjamin-bilal-perez@users.noreply.github.com>
2020-12-06 19:34:28 +0000

    Update sprinkler.ts

--- src/sprinkler.ts
+++ src/sprinkler.ts
@@ -3,12 +3,14 @@ import { ISprinkler } from "./iSprinkler";
 class Sprinkler implements ISprinkler {
     private _switchedOn: boolean = false;
 
-    turnOff() {
+    turnOff():number {
         this._switchedOn = false;
+        return 0;
     }
 
-    turnOn() {
+    turnOn():number {
         this._switchedOn = true;
+        return 5;
     }
 
     get switchedOn(): boolean {
