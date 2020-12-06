0ed1a15d57bd026f43bdd5db38edba18969ff1b7
Benjamín Bilal <56590175+benjamin-bilal-perez@users.noreply.github.com>
2020-12-06 19:32:16 +0000

    Update administrator.ts

--- src/administrator.ts
+++ src/administrator.ts
@@ -6,19 +6,25 @@ class Administrator implements IAdministrator {
     private _humidityObtained: number;
     private _sensor: ISensor;
     private _sprinkler: ISprinkler;
-    private _humidityOutput: HTMLElement = document.getElementById("humidity");
-    private _sprinklerOutput: HTMLElement = document.getElementById("sprinkler");
-    private _alertMessagesOutput: HTMLElement = document.getElementById("alert-messages");
+    private _fluctuation: number = 0;
+
+    private _humidityOutput: HTMLElement = document.getElementById("humidity-id");
+    private _sprinklerOutput: HTMLElement = document.getElementById("sprinkler-id");
+    private _alertMessagesOutput: HTMLElement = document.getElementById("alert-messages-id");
+    private _humidityBar:HTMLElement = document.getElementById("humidity-bar-id");
+    private _plant:HTMLElement = document.getElementById("plant-img-id");
+
 
     viewSensor() {
         this._humidityObtained = this._sensor.humidity;
+        this._humidityBar.style.width = this._sensor.humidity + "px";
     }
 
     turnOnSprinkler() {
-        this._sprinkler.turnOn();
+        this._fluctuation += this._sprinkler.turnOn();
     }
     turnOffSprinkler() {
-        this._sprinkler.turnOff();
+        this._fluctuation += this._sprinkler.turnOff();
     }
 
     manage(sprinkler: ISprinkler, sensor: ISensor) {
@@ -28,7 +34,10 @@ class Administrator implements IAdministrator {
     }
 
     check () {
+        this._fluctuation--;
+        this._sensor.fluctuation = this._fluctuation;
         this._sensor.read();
+        this._fluctuation = 0;
         this.viewSensor();
         this._alertMessagesOutput.innerHTML = "";
         this._sprinklerOutput.innerHTML = "";
@@ -38,19 +47,23 @@ class Administrator implements IAdministrator {
             this.turnOnSprinkler();
             this._sprinklerOutput.innerHTML = "Turning on the sprinkler...";
             this._alertMessagesOutput.innerHTML = "ALERT! LOW HUMIDITY LEVEL DETECTED";
+            (this._plant as HTMLImageElement).src = "imgs/BrownPlant.png";
         } else if (this._humidityObtained < 50) {
             this.turnOnSprinkler();
             this._sprinklerOutput.innerHTML = "Turning on the sprinkler...";
+            (this._plant as HTMLImageElement).src = "imgs/GreenPlant.png";
         } else if (this._humidityObtained > 70 && this._humidityObtained < 90) {
             this._sprinklerOutput.innerHTML = "Turning off the sprinkler...";
+            (this._plant as HTMLImageElement).src = "imgs/GreenPlant.png";
             this.turnOffSprinkler();
         } else if (this._humidityObtained > 80) {
             this.turnOffSprinkler();
             this._sprinklerOutput.innerHTML = "Turning off the sprinkler...";
             this._alertMessagesOutput.innerHTML = "ALERT! HIGH HUMIDITY LEVEL DETECTED";
+            (this._plant as HTMLImageElement).src = "imgs/UnhealthyPlant.png";
         }
 
-        setTimeout(this.check.bind(this), 3000);
+        setTimeout(this.check.bind(this), 1000);
     }
 
 }
